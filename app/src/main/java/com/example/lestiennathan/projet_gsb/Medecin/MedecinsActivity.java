fa89728dd41fd75eeddf5c7a7241e2d147b6e0a7
lestien.nathan <nath74340@live.fr>
2018-03-21 14:19:28 +0100

    répertoire et layout medecin

---
+++ app/src/main/java/com/example/lestiennathan/projet_gsb/Medecin/MedecinsActivity.java
@@ -0,0 +1,15 @@
+package com.example.lestiennathan.projet_gsb.Medecin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lestiennathan.projet_gsb.R;
+
+public class MedecinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medecins);
+    }
+}
---
+++ app/src/main/java/com/example/lestiennathan/projet_gsb/Metier/Medecin/MedecinAdapter.java
@@ -0,0 +1,8 @@
+package com.example.lestiennathan.projet_gsb.Metier.Medecin;
+
+/**
+ * Created by lestien.nathan on 21/03/2018.
+ */
+
+public class MedecinAdapter {
+}
